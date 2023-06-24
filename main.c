4cfcec9cc310ce25085522015af6fa28ef3f38ea
Matthew MacGregor <matsuzine>
2023-06-24 14:18:42 -0500

    Improved input error handling.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -29,21 +30,40 @@ void gen_random(const int len, char* buffer) {
     buffer[len] = 0; // null term
 }
 
+int is_numeric_str(const char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+	if (!isdigit(str[i])) {
+	    return 0;
+	}
+    }
+
+    return 1;
+}
+
 int main(int argc, char *argv[]) {
     int len = DEFAULT_LEN;
     size_t option_idx;
-    for (option_idx = 1; option_idx < argc && argv[option_idx][0] == '-'; option_idx++) {
+    for (option_idx = 1; option_idx < argc; option_idx++) {
         switch (argv[option_idx][1]) {
             case 'l':
-                len = atoi(argv[++option_idx]);
+		if (option_idx + 1 >= argc) {
+		    printf("Missing integer argument for option -l\n");
+		    exit(EXIT_FAILURE);
+		}
+                char* maybe_len = argv[++option_idx];
+		len = atoi(maybe_len);
                 if (len <= 0) {
-                    printf("%d is an invalid length\n", len);
+		    if (is_numeric_str(maybe_len) == 0) {
+		        printf("%s is not a valid value\n", maybe_len);
+		    } else {
+                        printf("%d is an invalid length\n", len);
+		    }
                     exit(EXIT_FAILURE);
                 }
                 len = CLAMP_MAX(len, ID_MAX_LEN);
             break;
             default:
-                fprintf(stderr, "Usage: %s [-i len]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-l len]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
